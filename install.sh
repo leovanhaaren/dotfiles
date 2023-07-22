381ece29ec62a1e17c3ff65ef9411c7c1c52313b
Leo van Haaren <814676+leovanhaaren@users.noreply.github.com>
2023-07-22 13:16:24 +0200

    fix: install script

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 ln -s $HOME/dotfiles/.git/hooks/pre-commit .git/hooks/pre-commit
-rcup
+
+cp $HOME/dotfiles/rcrc ~/.rcrc
+cd $HOME/dotfiles || exit
+
+rcup -v
+source $HOME/.zshrc
+
+reload
