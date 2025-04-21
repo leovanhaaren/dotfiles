e49493bb8373afc12c972584c5439209141dc089
Leo van Haaren <814676+leovanhaaren@users.noreply.github.com>
2025-04-21 19:05:44 +0200

    feat: streamline VSCode install script by removing commented-out lines and retaining essential commands

--- install-vscode.sh
+++ install-vscode.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# Install Homebrew if not installed
-# sh installers/brew.sh
+curl -sS https://starship.rs/install.sh | sh
 
-# git clone https://github.com/dracula/zsh.git "$ZSH_CUSTOM/themes/dracula-prompt"
-# ln -s "$ZSH_CUSTOM/themes/dracula-prompt/dracula.zsh-theme" "$ZSH_CUSTOM/themes/dracula.zsh-theme"
-
-# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# ln -s $HOME/dotfiles/.git/hooks/pre-commit .git/hooks/pre-commit
-
-# cp $HOME/dotfiles/rcrc ~/.rcrc
-# cd $HOME/dotfiles || exit
-
-# rcup -v -y
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 #symlink .zshrc
 cp $HOME/dotfiles/zshrc $HOME/.zshrc
