6a57ade18e76008a2960e30f709cae430be90f16
Leo van Haaren <814676+leovanhaaren@users.noreply.github.com>
2026-03-23 13:47:42 +0100

    chore(mac): simplify setup script and add gist credential helper
    - Disable most macOS system preferences defaults in install/mac.sh
    - Update workspace directory path to user-specific location
    - Add GitHub gist credential helper configuration for gh CLI auth

--- install/mac.sh
+++ install/mac.sh
@@ -14,106 +14,106 @@ osascript -e 'tell application "System Preferences" to quit'
 sudo -v
 
 echo "# Creating folders"
-mkdir -p ~/Workspaces
+mkdir -p ~/Workspaces/leovanhaaren/dotfiles
 mkdir -p ~/Screenshots
 
-echo "Finder: show all filename extensions"
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# echo "Finder: show all filename extensions"
+# defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-echo "show hidden files by default"
-defaults write com.apple.Finder AppleShowAllFiles -bool true
+# echo "show hidden files by default"
+# defaults write com.apple.Finder AppleShowAllFiles -bool true
 
-echo "only use UTF-8 in Terminal.app"
-defaults write com.apple.terminal StringEncodings -array 4
+# echo "only use UTF-8 in Terminal.app"
+# defaults write com.apple.terminal StringEncodings -array 4
 
-echo "show the ~/Library folder in Finder"
-chflags nohidden ~/Library
+# echo "show the ~/Library folder in Finder"
+# chflags nohidden ~/Library
 
-echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+# echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+# defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-echo "Enable subpixel font rendering on non-Apple LCDs"
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
+# echo "Enable subpixel font rendering on non-Apple LCDs"
+# defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
-echo "Enable automatically hide and show the Dock"
-defaults write com.apple.dock autohide -bool true
+# echo "Enable automatically hide and show the Dock"
+# defaults write com.apple.dock autohide -bool true
 
-echo "Enable Show remaining battery time;"
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-defaults write com.apple.menuextra.battery ShowTime -string "NO"
+# echo "Enable Show remaining battery time;"
+# defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+# defaults write com.apple.menuextra.battery ShowTime -string "NO"
 
-echo "Use current directory as default search scope in Finder"
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+# echo "Use current directory as default search scope in Finder"
+# defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-echo "Show Path bar in Finder"
-defaults write com.apple.finder ShowPathbar -bool true
+# echo "Show Path bar in Finder"
+# defaults write com.apple.finder ShowPathbar -bool true
 
-echo "Show Status bar in Finder"
-defaults write com.apple.finder ShowStatusBar -bool true
+# echo "Show Status bar in Finder"
+# defaults write com.apple.finder ShowStatusBar -bool true
 
-echo "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 1
+# echo "Set a blazingly fast keyboard repeat rate"
+# defaults write NSGlobalDomain KeyRepeat -int 1
 
-echo "Set a shorter Delay until key repeat"
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
+# echo "Set a shorter Delay until key repeat"
+# defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-echo "disabling smart quotes and dashes..."
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+# echo "disabling smart quotes and dashes..."
+# defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+# defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
-echo "Set date display in menu bar"
-defaults write com.apple.menuextra.clock "DateFormat" "EEE MMM d  H.mm"
+# echo "Set date display in menu bar"
+# defaults write com.apple.menuextra.clock "DateFormat" "EEE MMM d  H.mm"
 
-echo "Expand save panel by default"
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+# echo "Expand save panel by default"
+# defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+# defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
-echo "Expand print panel by default"
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+# echo "Expand print panel by default"
+# defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+# defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-echo "Disable the “Are you sure you want to open this application?” dialog"
-defaults write com.apple.LaunchServices LSQuarantine -bool false
+# echo "Disable the “Are you sure you want to open this application?” dialog"
+# defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-echo "Disable smart quotes as they’re annoying when typing code"
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+# echo "Disable smart quotes as they’re annoying when typing code"
+# defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
-echo "Stop iTunes from responding to the keyboard media keys"
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+# echo "Stop iTunes from responding to the keyboard media keys"
+# launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
 
-echo "Hide all desktop icons because who need 'em'"
-defaults write com.apple.finder CreateDesktop -bool false
+# echo "Hide all desktop icons because who need 'em'"
+# defaults write com.apple.finder CreateDesktop -bool false
 
-echo "Remove delay when taking a screenshot"
-defaults write com.apple.screencapture show-thumbnail -bool false
+# echo "Remove delay when taking a screenshot"
+# defaults write com.apple.screencapture show-thumbnail -bool false
 
-echo "Store screenshots in /Screenshots"
-defaults write com.apple.screencapture location /Screenshots
+# echo "Store screenshots in /Screenshots"
+# defaults write com.apple.screencapture location /Screenshots
 
-echo "Hide 'recent applications' from dock"
-defaults write com.apple.dock show-recents -bool false
+# echo "Hide 'recent applications' from dock"
+# defaults write com.apple.dock show-recents -bool false
 
-echo "Show bluetooth and other in the menubar"
-defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/AirPort.menu" "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" "/System/Library/CoreServices/Menu Extras/Displays.menu" "/System/Library/CoreServices/Menu Extras/Volume.menu"
+# echo "Show bluetooth and other in the menubar"
+# defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/AirPort.menu" "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" "/System/Library/CoreServices/Menu Extras/Displays.menu" "/System/Library/CoreServices/Menu Extras/Volume.menu"
 
-echo "Only show active apps in dock"
-defaults write com.apple.dock static-only -bool true; killall Dock
+# echo "Only show active apps in dock"
+# defaults write com.apple.dock static-only -bool true; killall Dock
 
-echo "Disable CMD+space for spotlight"
-/usr/libexec/PlistBuddy ~/Library/Preferences/com.apple.symbolichotkeys.plist -c "Set AppleSymbolicHotKeys:64:enabled false"
+# echo "Disable CMD+space for spotlight"
+# /usr/libexec/PlistBuddy ~/Library/Preferences/com.apple.symbolichotkeys.plist -c "Set AppleSymbolicHotKeys:64:enabled false"
 
-echo "Always show scrollbars"
-defaults write -g AppleShowScrollBars -string "Always"
+# echo "Always show scrollbars"
+# defaults write -g AppleShowScrollBars -string "Always"
 
-echo "Speed up dock show/hide"
-defaults write com.apple.dock autohide-delay -float 0;
-defaults write com.apple.dock autohide-time-modifier -int 0;
+# echo "Speed up dock show/hide"
+# defaults write com.apple.dock autohide-delay -float 0;
+# defaults write com.apple.dock autohide-time-modifier -int 0;
 
-# Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
+# # Disable the sound effects on boot
+# sudo nvram SystemAudioVolume=" "
 
-echo "Update Apple developer utils"
-softwareupdate --all --install --force
+# echo "Update Apple developer utils"
+# softwareupdate --all --install --force
 
-killall SystemUIServer
-killall "Dock"
+# killall SystemUIServer
+# killall "Dock"
